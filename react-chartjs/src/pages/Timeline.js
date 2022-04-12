20fa78d4d9723d0b2cee549b9c5882168c852052
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-04-12 23:16:14 +0200

    FIX & ADD: Timeline Historical ODDS data
    See description on how Odds in MMA work.

--- react-chartjs/src/pages/Timeline.js
+++ react-chartjs/src/pages/Timeline.js
@@ -1,60 +1,79 @@
 import { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import { getEvent, getSchedule } from "../services/ApiService";
+import { Dropdown, DropdownButton, Form, Button } from "react-bootstrap";
 import '../styles/Timeline.css'
 export function Timeline() {
     const [chartOptions, setChartOptions] = useState({});
     const [chartData, setChartData] = useState({
         datasets: [],
     });
+    const [scheduleData, setScheduleData] = useState([]);
+    const [selectedEvent, setSelectedEvent] = useState({
+        name: 'Select Event'
+    });
 
     useEffect(() => {
-        var fightDiffs = [];
-        var fights = [];
-        var fightCount = 0;
         getSchedule(2022).then(schedule => {
-            getEvent(schedule[0].EventId).then(event => {
-                // console.log(event);
-                event.Fights.forEach(fight => {
-                    fightCount++;
-                    fights.push(`Fight: ${fightCount}`);
-                    fightDiffs.push(fight.Fighters[0].Moneyline - fight.Fighters[1].Moneyline);
-                });
-
-                setChartData({
-                    labels: event.Fights.map(fight => `${fight.Fighters[0].LastName} vs ${fight.Fighters[1].LastName}`),
-                    datasets: [
-                        {
-                            label: "Fighter 1",
-                            data: event.Fights.map(fight => fight.Fighters[0].Moneyline),
-                            backgroundColor: "#E31C25",
-                        },
-                        {
-                            label: "Fighter 2",
-                            data: event.Fights.map(fight => fight.Fighters[1].Moneyline),
-                            backgroundColor: "#2b2b2b",
-                        },
-                    ]
-                });
-
-                setChartOptions({
-                    responsive: true,
-                    plugins: {
-                        title: {
-                            display: true,
-                            text: "UFC 2022 Fight Odd Differences",
-                        },
-                    },
-                });
-            })
+            const finalFights = schedule.filter(event => event.Status == "Final");
+            setScheduleData(finalFights);
         })
     }, [])
+
+    const handleSelect = (eventId) => {
+        var fightDiffs = [];
+        getEvent(eventId).then(event => {
+            console.log(event);
+
+            setSelectedEvent({
+                name: `${event.Name}`
+            });
+
+            const filteredFights = event.Fights.filter(fight => fight.Fighters.length == 2);
+
+            filteredFights.forEach(fight => {
+                // if (fight.Fighters.length == 2) {
+                //     console.log(fight);
+                //     fightDiffs.push(fight.Fighters[0].Moneyline - fight.Fighters[1].Moneyline);
+                // }
+                fightDiffs.push(fight.Fighters[0].Moneyline - fight.Fighters[1].Moneyline);
+            });
+
+            setChartData({
+                labels: filteredFights.map(fight => `${fight.Fighters[0].LastName}(1) vs ${fight.Fighters[1].LastName}(2)`),
+                datasets: [
+                    {
+                        label: "Fighter 1",
+                        data: filteredFights.map(fight => fight.Fighters[0].Moneyline),
+                        backgroundColor: "#E31C25",
+                    },
+                    {
+                        label: "Fighter 2",
+                        data: filteredFights.map(fight => fight.Fighters[1].Moneyline),
+                        backgroundColor: "#2b2b2b",
+                    },
+                ]
+            });
+
+            setChartOptions({
+                responsive: true,
+                plugins: {
+                    title: {
+                        display: false,
+                    },
+                },
+            });
+        })
+    }
 
     return (
         <div>
             <h1>See how evenly each fight was matched</h1>
             <p>UFC moneyline betting simply comes down to betting on who you believe is going to win the fight. For example, Conor McGregor was a -140 favorite for his fight vs Eddie Alvarez</p>
             <p>If the line is open, there was no moneyline given to the fighters</p>
+            <DropdownButton onSelect={selection => handleSelect(selection)} id="dropdown-basic-button" title={selectedEvent.name}>
+                {scheduleData.map(event => <Dropdown.Item eventKey={event.EventId} key={event.EventId}>{event.Name}</Dropdown.Item>)}
+            </DropdownButton>
             <div className="linechart">
                 <Line options={chartOptions} data={chartData} />
             </div>
